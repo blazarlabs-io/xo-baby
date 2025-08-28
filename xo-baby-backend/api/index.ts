edcf892b32c318d5c1ff053abaad52f59b68c755
ChizimInc <cezarchizim4@gmail.com>
2025-08-28 18:02:17 +0300

    change /api/index

--- xo-baby-backend/api/index.ts
+++ xo-baby-backend/api/index.ts
@@ -1,2 +1,28 @@
-// Point the Vercel serverless function to your compiled Nest handler
-module.exports = require('../dist/serverless.js').default;
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+
+const appPromise = (async () => {
+  const expressApp = express();
+  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+  nestApp.enableCors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
+  nestApp.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await nestApp.init();
+
+  return expressApp;
+})();
+
+
+export default async function handler(req: any, res: any) {
+  const app = await appPromise;
+  return (app as any)(req, res);
+}
