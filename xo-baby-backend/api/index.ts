f76444506784f2c44441ab21983fcfb09b5d39ec
ChizimInc <cezarchizim4@gmail.com>
2025-08-29 11:32:00 +0300

    add test ping api

--- xo-baby-backend/api/index.ts
+++ xo-baby-backend/api/index.ts
@@ -2,12 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import * as express from 'express'; // sigur pt TS fără esModuleInterop
 
 const appPromise = (async () => {
   const expressApp = express();
   const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-  nestApp.enableCors({ origin: '*', methods: ['GET','POST','PUT','DELETE','OPTIONS'], credentials: true });
+
+  nestApp.enableCors({
+    origin: '*',
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    credentials: true,
+  });
+
   nestApp.useGlobalPipes(new ValidationPipe({ transform: true }));
   await nestApp.init();
   return expressApp;
